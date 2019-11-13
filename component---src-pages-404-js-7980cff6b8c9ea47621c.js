32272ff0b3f66876bd03121738d4bcb859deb6aa
Zakaria Abdelaziz <zikk.abdelaziz@gmail.com>
2019-11-13 01:13:28 +0100

    Updates

---
+++ component---src-pages-404-js-7980cff6b8c9ea47621c.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{w2l6:function(e,n,t){"use strict";t.r(n);var a=t("q1tI"),l=t.n(a),r=t("+ZDr"),o=t.n(r);n.default=function(){return l.a.createElement("div",null,l.a.createElement("p",null,"404. Nothing to see here."),l.a.createElement("p",null,l.a.createElement(o.a,{to:"/"},"Home")))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-7980cff6b8c9ea47621c.js.map
--- src/components/tech-list.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default ({techs}) => (
-    <p className="tech-list">
-        {techs.map(techName => <span className="tech-item">{techName}</span>)}
-    </p>
-)
